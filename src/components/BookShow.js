62bf8a44076c93f74f129ce7645d7ae120a2d2b6
HalldorjonD <77391518+derhalljon@users.noreply.github.com>
2022-12-08 15:28:50 +0000

    added edit and delete functionality

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -2,8 +2,7 @@ import { useState } from 'react';
 import BookEdit from './BookEdit';
 
 
-function BookShow({ book, onDelete }) {
-
+function BookShow({ book, onDelete, onEdit }) {
     const [showEdit, setShowEdit] = useState(false);
 
     const handleDeleteClick = () => {
@@ -16,14 +15,21 @@ function BookShow({ book, onDelete }) {
 
     };
 
+    const handleSubmit = (id, newTitle) => {
+        setShowEdit(false);
+        onEdit(id, newTitle);
+
+    };
+
     let content = <h3>{book.title}</h3>;
     if (showEdit) {
-        content = <BookEdit />;
+        content = <BookEdit onSubmit={handleSubmit} book={book} />;
 
     };
 
     return (
         <div className="book-show">
+            <img src={`https://picsum.photos/seed/${book.id}/300/200`} alt="books" />
             <div>{content}</div>
             <div className="actions">
                 <button className="edit" onClick={handleEditClick}>Edit</button>
